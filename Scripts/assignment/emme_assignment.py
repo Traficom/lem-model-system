1dcd858d1b153e9ec9d2e721d4a741f6351930c2
Eeva Elmnäinen <ext-eeva.elmnainen@traficom.fi>
2023-02-02 15:11:40 +0200

    added set_stop_code

--- Scripts/assignment/emme_assignment.py
+++ Scripts/assignment/emme_assignment.py
@@ -1,6 +1,7 @@
 import numpy
 import pandas
 from math import log10
+from typing import NamedTuple, Dict
 
 import utils.log as log
 from utils.zone_interval import belongs_to_area, faulty_kela_code_nodes
@@ -45,6 +46,7 @@ class EmmeAssignmentModel(AssignmentModel):
 
     def prepare_network(self, car_dist_unit_cost=None):
         """Create matrices, extra attributes and calc background variables."""
+        self._set_stop_code()
         self._add_bus_stops()
         if self.separate_emme_scenarios:
             self.day_scenario = self.emme_project.copy_scenario(
@@ -301,30 +303,47 @@ class EmmeAssignmentModel(AssignmentModel):
         """
         return "@{}_{}".format(attr, "vrk")
 
+    def _set_stop_code(self):
+        class StopStats(NamedTuple):
+            stop_counts: int
+            stop_ratio: float
+            is_stop: bool
+
+        STOP_TYPE_ATTRIBUTE = '@stop_type'
+        KEEP_STOP_ATTRIBUTE = '@keep_stops'
+
+        self.mod_scenario.create_extra_attribute('NODE',STOP_TYPE_ATTRIBUTE)
+        self.mod_scenario.create_extra_attribute('TRANSIT_LINE',KEEP_STOP_ATTRIBUTE)
+
+        network = self.mod_scenario.get_network()
+        for node in network.nodes():
+            stops: Dict[str, StopStats] = {}
+            for mode in ['e', 'b']:
+                segments = [segment.allow_boardings or segment.allow_alightings for segment in node.outgoing_segments(include_hidden=True) if segment.line.mode.id==mode]
+                stop_counts = sum(segments)
+                stop_ratio = 0.0 if stop_counts < 1 else stop_counts/len(segments)
+                stops[mode] = StopStats(
+                    stop_counts, 
+                    stop_ratio,
+                    stop_ratio > 0.49 or stop_counts > 9)
+            node[STOP_TYPE_ATTRIBUTE] = 0
+            if stops['b'].is_stop:
+                node[STOP_TYPE_ATTRIBUTE] = 1
+            if stops['e'].is_stop:
+                node[STOP_TYPE_ATTRIBUTE] = 2
+            if (stops['e'].is_stop) and (stops['b'].is_stop):
+                node[STOP_TYPE_ATTRIBUTE] = 3
+        self.mod_scenario.publish_network(network)
+
     def _add_bus_stops(self):
         network = self.mod_scenario.get_network()
         for line in network.transit_lines():
-            if line.mode.id in param.stop_codes:
+            if line.mode.id in param.stop_codes and line["@keep_stops"] == 0:
                 stop_codes = param.stop_codes[line.mode.id]
                 for segment in line.segments():
                     is_stop = segment.i_node.data2 in stop_codes
-                    if line.mode.id in "de":
-                        # Non-HSL bus lines
-                        not_hsl = segment.i_node.label not in param.hsl_area
-                        if line.id[-1] == '1':
-                            # Line starts in HSL area
-                            segment.allow_alightings = not_hsl and is_stop
-                            segment.allow_boardings = is_stop
-                        elif line.id[-1] == '2':
-                            # Line ends in HSL area
-                            segment.allow_alightings = is_stop
-                            segment.allow_boardings = not_hsl and is_stop
-                        else:
-                            raise ValueError(
-                                "Unknown direction code for line " + line.id)
-                    else:
-                        segment.allow_alightings = is_stop
-                        segment.allow_boardings = is_stop
+                    segment.allow_alightings = is_stop
+                    segment.allow_boardings = is_stop
         self.mod_scenario.publish_network(network)
 
     def _create_attributes(self, scenario, extra):
