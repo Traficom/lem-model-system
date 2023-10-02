152e10a8baca26d7b72095d1ee97d41960ec7024
Jens West <jens.west@traficom.fi>
2023-10-02 14:23:19 +0300

    Adjust model result test according to removed transit cost adjustment

--- Scripts/tests/integration/test_models.py
+++ Scripts/tests/integration/test_models.py
@@ -53,7 +53,7 @@ class ModelTest(unittest.TestCase):
         self._validate_impedances(impedance["iht"])
 
         # Check that model result does not change
-        self.assertAlmostEquals(model.mode_share[0]["car"], 0.19396692706025762)
+        self.assertAlmostEquals(model.mode_share[0]["car"], 0.1996802102899737)
         
         print("Model system test done")
     
