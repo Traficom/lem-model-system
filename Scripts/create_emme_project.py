ce4e88c4015133856eba867c1911a3072f1f2c94
Krötzl Julius <Julius.Krotzl@traficom.fi>
2024-02-28 12:15:38 +0200

    Pull request #53: Create emme project
    Merge in VLE/model-system from create_emme_project to main
    
    Squashed commit of the following:
    
    commit 7919a1eab58bef7ff61cf7548ae011da4476322c
    Author: JULIUS KRÖTZL <julius.krotzl@traficom.fi>
    Date:   Wed Feb 28 12:14:57 2024 +0200
    
        Increase number of regular nodes in koko_suomi
    
    commit dff525b7166cca39cee48444f92c8c472c50a8ec
    Author: JULIUS KRÖTZL <julius.krotzl@traficom.fi>
    Date:   Wed Feb 28 12:10:49 2024 +0200
    
        Increase number of transit vehicles
    
    commit 43642121fcc2b28c60e03963829aa37bcd9217cc
    Author: JULIUS KRÖTZL <julius.krotzl@traficom.fi>
    Date:   Tue Feb 20 10:12:14 2024 +0200
    
        Removed the except when creating project
    
    commit 20c4d0175abe7bc0a675372d12eed0872b688bd5
    Author: JULIUS KRÖTZL <julius.krotzl@traficom.fi>
    Date:   Tue Feb 20 09:44:06 2024 +0200
    
        Modified EMME project path
    
    commit 78cddf577cc5d3005e80f4918b8721f4fa004834
    Author: JULIUS KRÖTZL <julius.krotzl@traficom.fi>
    Date:   Mon Feb 19 13:42:24 2024 +0200
    
        Removed unused arguments
    
    commit 6f27c49523fd6d6c5522f58916c1e25ef06ecab8
    Author: JULIUS KRÖTZL <julius.krotzl@traficom.fi>
    Date:   Mon Feb 19 13:21:57 2024 +0200
    
        Changed number of emme scenarios variable name
    
    commit 0d8f38b3d84245f1937ff39dea01e02f651f308c
    Author: JULIUS KRÖTZL <julius.krotzl@traficom.fi>
    Date:   Mon Feb 12 10:05:45 2024 +0200
    
        Replaced long if elif conditions with shorter ones
    
    commit a67b7f0644a71f778b99a13146417e08c892fc57
    Author: JULIUS KRÖTZL <julius.krotzl@traficom.fi>
    Date:   Mon Feb 12 09:12:20 2024 +0200
    
        Added args.scenario_number as variable to all submodels
    
    commit f371d56f3b8964a18b28d51f3d3fd6cb7ce74e8e
    Author: JULIUS KRÖTZL <julius.krotzl@traficom.fi>
    Date:   Mon Feb 12 09:04:13 2024 +0200
    
        Added number of scenarios as a variable to args
    
    commit 56519a7824b7daff6f423e4f39af6afd238f26ff
    Author: West Jens <Jens.West@traficom.fi>
    Date:   Mon Feb 12 08:47:21 2024 +0200
    
        Applied suggestion
    
    commit 5a1799c8865e4a364801f2950d029c4ccae1ec58
    Author: JULIUS KRÖTZL <julius.krotzl@traficom.fi>
    Date:   Mon Feb 12 08:40:41 2024 +0200
    
        Removed logging
    
    commit 9f03c2d679c73cba51f80a826f81750042462be3
    Author: JULIUS KRÖTZL <julius.krotzl@traficom.fi>
    Date:   Mon Feb 12 08:11:36 2024 +0200
    
        Changed project name as submodel
    
    commit 59777b0f3dc7ad9c29bd40c17fe4821cf41fc7b5
    Author: JULIUS KRÖTZL <julius.krotzl@traficom.fi>
    Date:   Mon Feb 12 07:41:56 2024 +0200
    
        Removed some unused variables
    
    commit a4e974c66d2b685080d19151992a6d8c8c939269
    Author: JULIUS KRÖTZL <julius.krotzl@traficom.fi>
    Date:   Fri Feb 9 15:20:21 2024 +0200
    
        Removed some unused variables
    
    commit 9aa9a0c77dff3b3eef8705ffb9e390588991ba8f
    Author: Mäkinen Sami <ext-sami.makinen@traficom.fi>
    Date:   Fri Feb 9 13:46:57 2024 +0200
    
        Applied suggestion
    
    commit 35a7cb5679afb1d3f8ee8da063c4120003d26c97
    Author: JULIUS KRÖTZL <julius.krotzl@traficom.fi>
    Date:   Tue Feb 6 15:57:31 2024 +0200
    
        Removed extra attribute creation and test assignments
    
    commit 95a7a3cd76768d7adb590fcb487fbd99812b93d4
    Author: JULIUS KRÖTZL <julius.krotzl@traficom.fi>
    Date:   Tue Feb 6 14:14:30 2024 +0200
    
        Add create_emme_project.py

---
+++ Scripts/create_emme_project.py
@@ -0,0 +1,137 @@
+from argparse import ArgumentParser
+import sys
+import os
+import logging
+
+import utils.config
+import utils.log as log
+import assignment.emme_assignment as ass
+from assignment.emme_bindings.emme_project import EmmeProject
+import inro.emme.desktop.app as _app
+import inro.emme.database.emmebank as _eb
+
+def create_emme_project(args):
+    logging.basicConfig(format='%(asctime)s %(message)s',
+                            datefmt='%Y-%m-%d %H:%M:%S',
+                            level=logging.INFO)
+    project_dir = args.emme_path
+    project_name = args.submodel
+    db_dir = os.path.join(project_dir, project_name, "Database")
+    project_path = _app.create_project(project_dir, project_name)
+    os.makedirs(db_dir)
+    # except FileExistsError:
+    #     project_path = os.path.join(
+    #         project_dir, project_name, project_name + ".emp")
+    default_dimensions = {
+        "scalar_matrices": 100, 
+        "origin_matrices": 100, 
+        "destination_matrices": 100, 
+        "full_matrices": 600, 
+        "scenarios": args.number_of_emme_scenarios, 
+        "centroids": 10000, 
+        "regular_nodes": 989999,
+        "links": 2000000, 
+        "turn_entries": 1, 
+        "transit_vehicles": 40, 
+        "transit_lines": 40000, 
+        "transit_segments": 2000000, 
+        "extra_attribute_values": 24120015,
+        "functions": 99, 
+        "operators": 5000, 
+        "sola_analyses": 240,
+    }
+    submodel_dimensions = {
+        "uusimaa": {
+            "centroids": 3000, 
+            "regular_nodes": 40000, 
+            "links": 150000,
+            "transit_lines": 3000,
+            "transit_segments": 250000,
+            "extra_attribute_values": 30000000,
+        },
+        "lounais_suomi": {
+            "centroids": 3000, 
+            "regular_nodes": 45000, 
+            "links": 150000,
+            "transit_lines": 3000,
+            "transit_segments": 250000,
+            "extra_attribute_values": 30000000,            
+        },
+        "ita_suomi": {
+            "centroids": 3300, 
+            "regular_nodes": 45000, 
+            "links": 150000,
+            "transit_lines": 3300,
+            "transit_segments": 300000,
+            "extra_attribute_values": 30000000,            
+        },
+        "pohjois_suomi": {
+            "centroids": 3000, 
+            "regular_nodes": 40000, 
+            "links": 150000,
+            "transit_lines": 2500,
+            "transit_segments": 200000,
+            "extra_attribute_values": 30000000,                  
+        },
+        "koko_suomi": {
+            "centroids": 10000, 
+            "regular_nodes": 120000, 
+            "links": 350000,
+            "transit_lines": 8000,
+            "transit_segments": 700000,
+            "extra_attribute_values": 70000000,                  
+        }
+    }
+    dim = {**default_dimensions, **submodel_dimensions[args.submodel]}
+    scenario_num = args.first_scenario_id
+    eb = _eb.create(os.path.join(db_dir, "emmebank"), dim)
+    eb.text_encoding = 'utf-8'
+    scen = eb.create_scenario(scenario_num)
+    emmebank_path = eb.path
+    eb.dispose()
+    EmmeProject(project_path, emmebank_path)
+
+if __name__ == "__main__":
+    # Initially read defaults from config file ("dev-config.json")
+    # but allow override via command-line arguments
+    config = utils.config.read_from_file()
+    parser = ArgumentParser(epilog="HELMET model system entry point script.")
+    # Logging
+    parser.add_argument(
+        "--log-level",
+        choices={"DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"},
+        default=config.LOG_LEVEL,
+    )
+    parser.add_argument(
+        "--log-format",
+        choices={"TEXT", "JSON"},
+        default=config.LOG_FORMAT,
+    )
+    # HELMET scenario input data
+    parser.add_argument(
+        "--submodel",
+        type=str,
+        default=config.SUBMODEL,
+        help="Name of submodel, used for choosing appropriate database dimensions"),
+    parser.add_argument(
+        "--emme-path",
+        type=str,
+        default=config.EMME_PROJECT_PATH,
+        help="Filepath to folder where EMME project will be created"),
+    parser.add_argument(
+        "--number-of-emme-scenarios",
+        type=int,
+        default=config.NUMBER_OF_EMME_SCENARIOS,
+        help="Number of scenarios in the emmebank"),
+    parser.add_argument(
+        "--first-scenario-id",
+        type=int,
+        default=config.FIRST_SCENARIO_ID,
+        help="First EMME project scenario ID"),
+    args = parser.parse_args()
+
+    args_dict = vars(args)
+    for key in args_dict:
+        log.debug("{}={}".format(key, args_dict[key]))
+
+    create_emme_project(args)
