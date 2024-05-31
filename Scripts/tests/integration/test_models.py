9655ac09be458761c31f3378dc2aea4777968122
suppoatt <ext-atte.supponen@traficom.fi>
2024-05-31 14:03:34 +0300

    Update integration test mode share

--- Scripts/tests/integration/test_models.py
+++ Scripts/tests/integration/test_models.py
@@ -53,7 +53,7 @@ class ModelTest(unittest.TestCase):
         # Check that model result does not change
         self.assertAlmostEquals(
             model.mode_share[0]["car_work"] + model.mode_share[0]["car_leisure"],
-            0.24755222002919075)
+            0.23980001182734606)
         
         print("Model system test done")
     
