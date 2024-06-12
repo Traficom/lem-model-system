2bdb6a91490063e4b0c454fb95ddd8808bff6d8a
Tarkkala Konsta <tarkkkon@TRAFI.INTRA>
2024-06-12 10:33:12 +0300

    Save freight demand to omx. Changed freight extended transit assign perception factor from 10 to 30.

--- Scripts/tests/unit/test_freight_model.py
+++ Scripts/tests/unit/test_freight_model.py
@@ -4,6 +4,7 @@ import os
 import json
 import numpy
 import unittest
+import openmatrix as omx
 
 from datatypes.purpose import FreightPurpose
 from datahandling.zonedata import BaseZoneData
@@ -16,7 +17,7 @@ from utils.freight_costs import calc_rail_cost, calc_road_cost, calc_ship_cost
 
 TEST_DATA_PATH = os.path.join(
     os.path.dirname(os.path.realpath(__file__)), "..", "test_data")
-METROPOLITAN_ZONES = numpy.loadtxt(
+ZONE_NUMBERS = numpy.loadtxt(
     os.path.join(TEST_DATA_PATH, "Scenario_input_data",
                  "2030_test", "freight_zones.csv"))
 MODEL_TYPE = "dest_mode"
@@ -37,34 +38,34 @@ class FreightModelTest(unittest.TestCase):
                 with open(path + "\\" + v) as f:
                    freight_costdata[k] = json.load(f)
             return freight_costdata
-
+        wd = os.path.join(TEST_DATA_PATH, "Scenario_input_data", "2030_test")
         resultdata = ResultsData(os.path.join(TEST_DATA_PATH, "Results",
                                               "test", "freight", MODEL_TYPE))
         basezonedata = BaseZoneData(
-            os.path.join(TEST_DATA_PATH, "Scenario_input_data", "2030_test"),
-            numpy.array(METROPOLITAN_ZONES),
-            "koko_suomi_kunta.zmp")
+            wd, numpy.array(ZONE_NUMBERS), "koko_suomi_kunta.zmp")
         zonedata = ZoneData(
-            os.path.join(TEST_DATA_PATH, "Scenario_input_data", "2030_test"),
-            numpy.array(METROPOLITAN_ZONES),
-            basezonedata.aggregations,
-            "koko_suomi_kunta.zmp")
+            wd, numpy.array(ZONE_NUMBERS), basezonedata.aggregations, "koko_suomi_kunta.zmp")
         parameters_path = os.path.join(os.path.dirname(
                 os.path.realpath(__file__)), "..", "..", "parameters",
                 "freight", MODEL_TYPE)
         purposes = {}
+        with open(os.path.join(wd, "comm_finage_conversion.json")) as f:
+            finage_comms = json.load(f)
         for file_name in os.listdir(parameters_path):
             if file_name.endswith(".json"):
                 with open(os.path.join(parameters_path, file_name), 'r') as file:
-                    fname = file_name.split("_")[0]
-                    purposes[fname] = FreightPurpose(json.load(file), zonedata,
-                                                    resultdata)
+                    comm_params = json.load(file)
+                    comm_names = finage_comms[file_name.split("_")[0]]
+                    for comm in comm_names:
+                        purposes[comm] = FreightPurpose(comm_params, zonedata,
+                                                        resultdata)
         ass_model = EmmeAssignmentModel(
             EmmeProject("C:\\emmeproj\\freight_testing\\freight_testing.emp"), first_scenario_id=11)
         ass_model.prepare_freight_network(zonedata.car_dist_cost, list(purposes))
         temp_impedance = ass_model.freight_network.assign()
         freight_costdata = read_freight_costdata()
-        demand_list = {}
+        omx_file = omx.open_file(wd + "\\freight_demand_tons_year.omx", "w")
+        omx_file.create_mapping("zone_number", ZONE_NUMBERS)
         for purpose_key, purpose_value in purposes.items():
             impedance = {
                 "truck": {
@@ -90,9 +91,9 @@ class FreightModelTest(unittest.TestCase):
             }
             demand = purpose_value.calc_traffic(impedance, purpose_key)
             for mode in demand:
-                ass_model.freight_network.set_matrix(mode, demand[mode])
-            demand_list[purpose_key] = demand
-            ass_model.freight_network.save_network_volumes(purpose_key)
+                omx_file[f"{purpose_key}_{mode}"] = demand[mode]
+                #ass_model.freight_network.set_matrix(mode, demand[mode])
+            #ass_model.freight_network.save_network_volumes(purpose_key)
 
 test = FreightModelTest()
 test.test_freight_model()
