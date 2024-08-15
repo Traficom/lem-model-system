68ce3930ead91f3937014b4a8de7d57ca2a35290
Jens West <jens.west@traficom.fi>
2024-08-15 15:44:15 +0300

    Add possibility to add calibration parameters

--- Scripts/datatypes/purpose.py
+++ Scripts/datatypes/purpose.py
@@ -181,6 +181,7 @@ def new_tour_purpose(specification, zone_data, resultdata):
     resultdata : ResultData
         Writer object for result directory
     """
+    attempt_calibration(specification)
     args = (specification, zone_data, resultdata)
     purpose = (SecDestPurpose(*args) if "sec_dest" in specification
                 else TourPurpose(*args))
@@ -446,3 +447,31 @@ class SecDestPurpose(Purpose):
                 sum(self.attracted_tours.values()),
                 self.zone_data.zone_numbers, name=self.name),
             "attraction.txt")
+
+def attempt_calibration(spec: dict):
+    """Check if specification includes "calibration" trees.
+
+    Transform regular parameters to include calibration,
+    remove separate calibration parameters.
+    A calibration (named "calibration") tree can be anywhere in the dict,
+    but must refer to existing adjacent parameters or trees of parameters.
+    """
+    try:
+        param_names = list(spec.keys())
+    except AttributeError:
+        # No calibration parameters in this branch, return up
+        return
+    for param_name in param_names:
+        if param_name == "calibration":
+            calibrate(spec, spec.pop(param_name))
+        else:
+            # Search deeper
+            attempt_calibration(spec[param_name])
+
+def calibrate(spec: dict, calib_spec: dict):
+    for param_name in calib_spec:
+        try:
+            spec[param_name] += calib_spec[param_name]
+        except TypeError:
+            # Search deeper
+            calibrate(spec[param_name], calib_spec[param_name])
