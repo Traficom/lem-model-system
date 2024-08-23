91c9ef1ea1d58cb0c71e1d380f7fe98a7907b88d
attesn <atte.supponen@outlook.com>
2024-08-23 09:24:27 +0300

    Update integration test to match new parameters

--- Scripts/tests/integration/test_models.py
+++ Scripts/tests/integration/test_models.py
@@ -53,7 +53,7 @@ class ModelTest(unittest.TestCase):
         # Check that model result does not change
         self.assertAlmostEquals(
             model.mode_share[0]["car_work"] + model.mode_share[0]["car_leisure"],
-            0.20412287404369955)
+            0.19920193721251422)
         
         print("Model system test done")
     
