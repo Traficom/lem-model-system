b18a4d2064482323aa8f96d679720315728b17e4
attesn <atte.supponen@outlook.com>
2024-09-16 17:24:50 +0300

    Update mode shares in integration test

--- Scripts/tests/integration/test_models.py
+++ Scripts/tests/integration/test_models.py
@@ -53,7 +53,7 @@ class ModelTest(unittest.TestCase):
         # Check that model result does not change
         self.assertAlmostEquals(
             model.mode_share[0]["car_work"] + model.mode_share[0]["car_leisure"],
-            0.26238580233159714)
+            0.26634073564923444)
         
         print("Model system test done")
     
