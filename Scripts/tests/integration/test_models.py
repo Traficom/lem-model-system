c9c8a354072718f62356e3dff5c0371faad22697
attesn <atte.supponen@outlook.com>
2024-11-01 17:51:41 +0200

    Fix car passenger occupancy rate in matrix transformations

--- Scripts/tests/integration/test_models.py
+++ Scripts/tests/integration/test_models.py
@@ -54,7 +54,7 @@ class ModelTest(unittest.TestCase):
         # Check that model result does not change
         self.assertAlmostEquals(
             model.mode_share[0]["car_work"] + model.mode_share[0]["car_leisure"],
-            0.3068205936960563)
+            0.3015116522404117)
         
         print("Model system test done")
     
