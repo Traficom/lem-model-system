3c408f971eaab6536eba17a32b75f88124a3e111
attesn <atte.supponen@outlook.com>
2024-11-12 14:42:55 +0200

    Use consistent names in result files

--- Scripts/datatypes/histogram.py
+++ Scripts/datatypes/histogram.py
@@ -11,7 +11,7 @@ class TourLengthHistogram:
         index = [f"{i}-{j}" for i, j in zip(intervals[:-1], intervals[1:])]
         index.append("total_kms")
         self.histogram = pandas.concat(
-            {name: pandas.Series(0.0, index, name="nr_of_tours")})
+            {name: pandas.Series(0.0, index, name="nr_tours")})
 
     def add(self, dist):
         self.histogram.iat[numpy.searchsorted(self._u, dist, "right")] += 1
