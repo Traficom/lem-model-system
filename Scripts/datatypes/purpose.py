92284ce139b7df0dcec73931a81f0d9769b3c911
attesn <atte.supponen@outlook.com>
2024-11-13 17:14:01 +0200

    Scale upper level parameters with nest coefficients

--- Scripts/datatypes/purpose.py
+++ Scripts/datatypes/purpose.py
@@ -197,6 +197,7 @@ def new_tour_purpose(*args):
         Dict of matrix adjustments for testing elasticities
     """
     specification = args[0]
+    attempt_scaling(specification)
     attempt_calibration(specification)
     if "sec_dest" in specification:
         purpose = SecDestPurpose(*args)
@@ -527,6 +528,26 @@ def attempt_calibration(spec: dict):
             # Search deeper
             attempt_calibration(spec[param_name])
 
+def attempt_scaling(spec: dict):
+    """Check if specification includes "calibration" trees.
+
+    Transform regular parameters to include calibration,
+    remove separate calibration parameters.
+    A calibration (named "calibration") tree can be anywhere in the dict,
+    but must refer to existing adjacent parameters or trees of parameters.
+    """
+    try:
+        param_names = list(spec.keys())
+    except AttributeError:
+        # No calibration parameters in this branch, return up
+        return
+    for param_name in param_names:
+        if param_name == "scaling":
+            scale(spec, spec.pop(param_name))
+        else:
+            # Search deeper
+            attempt_scaling(spec[param_name])
+
 def calibrate(spec: dict, calib_spec: dict):
     for param_name in calib_spec:
         try:
@@ -534,7 +555,14 @@ def calibrate(spec: dict, calib_spec: dict):
         except TypeError:
             # Search deeper
             calibrate(spec[param_name], calib_spec[param_name])
-            
+
+def scale(spec: dict, calib_spec: dict):
+    for param_name in calib_spec:
+        try:
+            spec[param_name] *= calib_spec[param_name]
+        except TypeError:
+            # Search deeper
+            scale(spec[param_name], calib_spec[param_name])
 
 class FreightPurpose(Purpose):
 
