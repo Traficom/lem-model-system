d762b873642c69e5c6182981129a5882a050723d
attesn <60136658+attesn@users.noreply.github.com>
2024-11-26 09:46:52 +0200

    Scale upper level parameters with nest coefficients (#112)
    * Scale upper level parameters with nest coefficients
    
    * Remove scaling from size parameters
    
    * Update integration test mode shares
    
    * Combine scaling to calibration method
    
    * Move size variables to nest specific utility in dest-mode struct

--- Scripts/tests/integration/test_models.py
+++ Scripts/tests/integration/test_models.py
@@ -54,7 +54,7 @@ class ModelTest(unittest.TestCase):
         # Check that model result does not change
         self.assertAlmostEquals(
             model.mode_share[0]["car_work"] + model.mode_share[0]["car_leisure"],
-            0.27293520807660276)
+            0.34376735936254194)
         
         print("Model system test done")
     
