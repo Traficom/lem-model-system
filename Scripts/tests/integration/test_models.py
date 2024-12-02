4eb93dee3a302f6aef5dad890fee3603014a9fe0
Mutikainen Juhani <mutikjuh@TRAFI.INTRA>
2024-12-02 20:48:13 +0200

    Update assertAlmostEquals test comparison value

--- Scripts/tests/integration/test_models.py
+++ Scripts/tests/integration/test_models.py
@@ -54,7 +54,7 @@ class ModelTest(unittest.TestCase):
         # Check that model result does not change
         self.assertAlmostEquals(
             model.mode_share[0]["car_work"] + model.mode_share[0]["car_leisure"],
-            0.34376735936254194)
+            0.34241463698614005)
         
         print("Model system test done")
     
