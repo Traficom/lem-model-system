db5518b213e87d77ae41a9c569db6900746720dd
Jens West <jens.west@traficom.fi>
2024-12-04 12:47:52 +0200

    Fix distance boundaries in `DestModeModel` (#124)
    * Fix distance boundaries in `DestModeModel`
    
    * Add comment

--- Scripts/tests/integration/test_models.py
+++ Scripts/tests/integration/test_models.py
@@ -54,7 +54,7 @@ class ModelTest(unittest.TestCase):
         # Check that model result does not change
         self.assertAlmostEquals(
             model.mode_share[0]["car_work"] + model.mode_share[0]["car_leisure"],
-            0.3543415640331298)
+            0.35433202534858543)
         
         print("Model system test done")
     
