a60c5e78f39fe6c11ef9783d1a4396df96b29176
Jens West <jens.west@traficom.fi>
2025-01-29 12:05:48 +0200

    Update test LOS matrices (#147)
    * Update test LOS matrices
    
    * Update test_models.py

--- Scripts/tests/integration/test_models.py
+++ Scripts/tests/integration/test_models.py
@@ -55,7 +55,7 @@ class ModelTest(unittest.TestCase):
         # Check that model result does not change
         self.assertAlmostEquals(
             model.mode_share[0]["car_work"] + model.mode_share[0]["car_leisure"],
-            0.33021994777224084)
+            0.33680336972774455)
         
         print("Model system test done")
     
