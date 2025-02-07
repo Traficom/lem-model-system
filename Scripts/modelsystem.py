7e85bbb4606489e8780417d4d54651fd8c94cfdb
Jens West <jens.west@traficom.fi>
2025-02-07 09:36:26 +0200

    Remove car_work and transit_work LOS matrices after usage

--- Scripts/modelsystem.py
+++ Scripts/modelsystem.py
@@ -88,9 +88,11 @@ class ModelSystem:
         self.resultdata = ResultsData(results_path)
         self.resultmatrices = MatrixData(results_path / "Matrices" / submodel)
         parameters_path = Path(__file__).parent / "parameters" / "demand"
-        home_based_purposes = []
+        home_based_work_purposes = []
+        home_based_leisure_purposes = []
         sec_dest_purposes = []
-        other_purposes = []
+        other_work_purposes = []
+        other_leisure_purposes = []
         for file in parameters_path.rglob("*.json"):
             purpose = new_tour_purpose(
                 json.loads(file.read_text("utf-8")), self.zdata_forecast,
@@ -101,11 +103,19 @@ class ModelSystem:
                 if isinstance(purpose, SecDestPurpose):
                     sec_dest_purposes.append(purpose)
                 elif purpose.orig == "home":
-                    home_based_purposes.append(purpose)
+                    if param.assignment_classes[purpose.name] == "work":
+                        home_based_work_purposes.append(purpose)
+                    else:
+                        home_based_leisure_purposes.append(purpose)
                 else:
-                    other_purposes.append(purpose)
+                    if param.assignment_classes[purpose.name] == "work":
+                        other_work_purposes.append(purpose)
+                    else:
+                        other_leisure_purposes.append(purpose)
         self.dm = self._init_demand_model(
-            home_based_purposes + other_purposes + sec_dest_purposes)
+            home_based_work_purposes + other_work_purposes
+            + home_based_leisure_purposes + other_leisure_purposes
+            + sec_dest_purposes)
         self.travel_modes = {mode: True for purpose in self.dm.tour_purposes
             for mode in purpose.modes}  # Dict instead of set, to preserve order
         self.em = ExternalModel(
@@ -146,6 +156,11 @@ class ModelSystem:
         # as logsums from probability calculation are used in tour generation.
         self.dm.create_population_segments()
         for purpose in self.dm.tour_purposes:
+            if param.assignment_classes[purpose.name] == "leisure":
+                for tp_imp in previous_iter_impedance.values():
+                    for imp in tp_imp.values():
+                        imp.pop("car_work", None)
+                        imp.pop("transit_work", None)
             purpose_impedance = purpose.calc_prob(
                 previous_iter_impedance, is_last_iteration)
         previous_iter_impedance.clear()
