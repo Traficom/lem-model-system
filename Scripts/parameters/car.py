36a2c6c8776517bab0c77aba5866c56a323ff9cf
jonisylvinwsp <joni.sylvin@wsp.com>
2025-02-26 15:21:15 +0200

    Update car choice model coefficients

--- Scripts/parameters/car.py
+++ Scripts/parameters/car.py
@@ -25,29 +25,33 @@ car_ownership = {
         "individual_dummy": {},
     },
     1: {
-        "constant": 3.182379,
+        "constant": 2.568245,
         "generation": {
-            "hb_leisure_sustainable": -0.417948,
-            "sh_row_or_detached": 1.284792,
+            "hb_leisure_sustainable": -0.476655,
+            "sh_row_or_detached": 1.116087,
         },
         "individual_dummy": {
-            "sh_income_0_19": -2.589644,
-            "sh_income_20_39": -1.030502,
+            "sh_income_0_19": -1.645065,
+            "sh_income_20_39": -0.167748,
+            "sh_income_40_59": 0.669643,
+            "sh_income_60_79": 1.235251,
+            "sh_income_80_99": 0.991755,
+            "sh_income_100_": 1.484410
         },
     },
     2: {
-        "constant": 3.257525,
+        "constant": 2.794911,
         "generation": {
-            "hb_leisure_sustainable": -1.170948,
-            "sh_row_or_detached": 1.284792,
+            "hb_leisure_sustainable": -1.061793,
+            "sh_row_or_detached": 2.127492,
         },
         "individual_dummy": {
-            "sh_income_0_19": -2.589644,
-            "sh_income_20_39": -1.030502,
-            "sh_income_40_59": 1.325116,
-            "sh_income_60_79": 1.964296,
-            "sh_income_80_99": 2.356314,
-            "sh_income_100_": 2.917251
+            "sh_income_0_19": -3.653949,
+            "sh_income_20_39": -1.191287,
+            "sh_income_40_59": 0.714940,
+            "sh_income_60_79": 1.869940,
+            "sh_income_80_99": 2.020383,
+            "sh_income_100_": 3.034884
         },
     }
 }
