369b51166742368283bed8e9d8e224109b63bd5d
jonisylvinwsp <joni.sylvin@wsp.com>
2025-02-26 15:33:39 +0200

    Set higher education parameters to zero and update subsequent mode share

--- Scripts/tests/integration/test_models.py
+++ Scripts/tests/integration/test_models.py
@@ -55,7 +55,7 @@ class ModelTest(unittest.TestCase):
         # Check that model result does not change
         self.assertAlmostEquals(
             model.mode_share[0]["car_work"] + model.mode_share[0]["car_leisure"],
-            0.33811383650783045)
+            0.33679049755452384)
         
         print("Model system test done")
     
