0a0b0eaf16e05ede846c87e922997a37cebb0088
attesn <atte.supponen@outlook.com>
2025-03-10 10:52:43 +0200

    Add car ownership and household dummy Replace car density in utility functions

--- Scripts/tests/integration/test_models.py
+++ Scripts/tests/integration/test_models.py
@@ -55,7 +55,7 @@ class ModelTest(unittest.TestCase):
         # Check that model result does not change
         self.assertAlmostEquals(
             model.mode_share[0]["car_work"] + model.mode_share[0]["car_leisure"],
-            0.33679049755452384)
+            0.16313375591382953)
         
         print("Model system test done")
     
