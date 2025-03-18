bfdd0c383e14c24dab19e73114bc9be7fd1d3269
attesn <atte.supponen@outlook.com>
2025-03-18 14:44:55 +0200

    Update test zonedata and integration test

--- Scripts/tests/integration/test_models.py
+++ Scripts/tests/integration/test_models.py
@@ -55,7 +55,7 @@ class ModelTest(unittest.TestCase):
         # Check that model result does not change
         self.assertAlmostEquals(
             model.mode_share[0]["car_work"] + model.mode_share[0]["car_leisure"],
-            0.31408963309396976)
+            0.2909955476784347)
         
         print("Model system test done")
     
