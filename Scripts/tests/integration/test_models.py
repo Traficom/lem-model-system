dfce5dd46a130866c4a8af462cf1a76d9a0e727d
attesn <atte.supponen@outlook.com>
2025-04-07 15:50:54 +0300

    Purpose specific distance boundaries

--- Scripts/tests/integration/test_models.py
+++ Scripts/tests/integration/test_models.py
@@ -54,7 +54,7 @@ class ModelTest(unittest.TestCase):
         # Check that model result does not change
         self.assertAlmostEquals(
             model.mode_share[0]["car_work"] + model.mode_share[0]["car_leisure"],
-            0.3306838374554306)
+            0.31711155476706987)
         
         print("Model system test done")
     
