596e11296774a55c35d1b808ec78cd43731b9bcd
jonisylvinwsp <joni.sylvin@wsp.com>
2025-04-17 10:11:23 +0300

    Add linear sustainable accessibility model and subsequent mode share change

--- Scripts/tests/integration/test_models.py
+++ Scripts/tests/integration/test_models.py
@@ -54,7 +54,7 @@ class ModelTest(unittest.TestCase):
         # Check that model result does not change
         self.assertAlmostEquals(
             model.mode_share[0]["car_work"] + model.mode_share[0]["car_leisure"],
-            0.3357713386140368)
+            0.335304538711925)
         
         print("Model system test done")
 
