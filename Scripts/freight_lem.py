fcbf0e2448ac98e24f1ab1fe395ea962073a2763
Konsta Tarkkala <konsta.tarkkala@wsp.com>
2025-04-24 16:12:55 +0300

    Fix EmmeAssignmentModel save matrices parameter to be based on if items exist in specify-commodity-names argument

--- Scripts/freight_lem.py
+++ Scripts/freight_lem.py
@@ -25,10 +25,10 @@ def main(args):
     results_path = Path(args.results_path, args.scenario_name)
     emme_project_path = Path(args.emme_path)
     parameters_path = Path(__file__).parent / "parameters" / "freight"
-    
+    save_matrices = True if args.specify_commodity_names else False
     ass_model = EmmeAssignmentModel(EmmeProject(emme_project_path),
                                     first_scenario_id=args.first_scenario_id,
-                                    save_matrices=args.save_emme_matrices,
+                                    save_matrices=save_matrices,
                                     first_matrix_id=args.first_matrix_id)
     zone_numbers = ass_model.zone_numbers
     zonedata = FreightZoneData(zonedata_path, zone_numbers, "koko_suomi")
