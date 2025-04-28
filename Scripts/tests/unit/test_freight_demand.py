9f308858e0ce251ea158d6d8e0dea98d47544d97
Konsta Tarkkala <konsta.tarkkala@wsp.com>
2025-04-28 15:43:11 +0300

    Fix freight demand test

--- Scripts/tests/unit/test_freight_demand.py
+++ Scripts/tests/unit/test_freight_demand.py
@@ -15,7 +15,7 @@ TEST_PATH = Path(__file__).parent.parent / "test_data"
 TEST_DATA_PATH = TEST_PATH / "Scenario_input_data"
 TEST_MATRICES = TEST_PATH / "Scenario_input_data" / "Matrices" / "uusimaa"
 RESULT_PATH = TEST_PATH / "Results"
-PARAMETERS_PATH = TEST_PATH.parent.parent / "parameters" / "freight"
+PARAMETERS_PATH = TEST_PATH.parent.parent / "parameters" / "freight" / "domestic"
 ZONE_NUMBERS = [202, 1344, 1755, 2037, 2129, 2224, 2333, 2413, 2519, 2621,
                 2707, 2814, 2918, 3000, 3003, 3203, 3302, 3416, 3639, 3705,
                 3800, 4013, 4101, 4202, 7043, 8284, 12614, 17278, 19419, 23678]
@@ -37,7 +37,8 @@ class FreightModelTest(unittest.TestCase):
                 purposes[commodity] = FreightPurpose(commodity_params,
                                                      zonedata,
                                                      resultdata,
-                                                     costdata["freight"][commodity_conversion[commodity]])
+                                                     costdata["freight"][commodity_conversion[commodity]],
+                                                     "domestic")
 
         time_impedance = omx.open_file(TEST_MATRICES / "freight_time.omx", "r")
         dist_impedance = omx.open_file(TEST_MATRICES / "freight_dist.omx", "r")
