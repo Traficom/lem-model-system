Traficom/lem-model-system

commit e0d5bccb0fcc15bb0e4fad3945b992b126a95401

Refactor `AssignmentModel` (#123)
* Draft AssignmentMode

* Add transit specification and setting matrices

* Add bike and walk modes

* Refactor assignments

* Fix assignments

* Fix mock assignment

* Move modes and remove matrix parameters

* Fix mock assignment

* Implement `save_matrices` argument and improve typing

* Fix transit assignment specification, freight assignment and matrices

* Release more matrices

* Clear truck matrices

* Refine transit handling

* Improve inits and add docstrings

* Fix toll_cost and inv_time handling

* Fix MockAssignment

* Simplify mode initialization and add class docstring

* Improve typing and docstrings

* Fix matrix id counter

* Simplify toll-cost handling

* Fix auxiliary apps

* Fix off-peak assignment in loop

* Add testing of all assignment periods

Scripts/cba.py
@@ -4,6 +4,7 @@ import numpy
 import pandas
 from pathlib import Path
 from openpyxl import load_workbook
+from tables.exceptions import NoSuchNodeError
 
 import utils.config
 import utils.log as log
@@ -259,10 +260,17 @@ def run_cost_benefit_analysis(scenario_0, scenario_1, year, workbook, submodel):
                     zone_numbers = mtx.zone_numbers
                 result_type = f"{transport_class}_demand_{scenario}"
                 results[result_type] += vol_fac * demand[scenario].sum(0)
-            for mtx_type in ["dist", "time", "cost"]:
-                cost = {scenario: read_costs(
-                        data[scenario], timeperiod, transport_class, mtx_type)
-                    for scenario in data}
+            for mtx_type in ["dist", "time", "cost", "toll_cost"]:
+                cost = {}
+                for scenario in data:
+                    with data[scenario].open(mtx_type, timeperiod) as mtx:
+                        try:
+                            cost[scenario] = mtx[transport_class]
+                            matrices_found = True
+                        except NoSuchNodeError:
+                            matrices_found = False
+                if not matrices_found:
+                    break
                 if transport_class == "train" and mtx_type == "dist":
                     # Max travel time with fixed 20 kmph speed
                     # and one-hour start time
@@ -274,17 +282,18 @@ def run_cost_benefit_analysis(scenario_0, scenario_1, year, workbook, submodel):
                 result_type = transport_class + "_" + mtx_type
                 results[result_type] += (vol_fac *
                                          (gains_existing+gains_additional))
-                ws = workbook[TRANSLATIONS[transport_class]]
-                ws[cols[timeperiod]+rows[mtx_type][0]] = gains_existing.sum()
-                ws[cols[timeperiod]+rows[mtx_type][1]] = gains_additional.sum()
                 if mtx_type == "cost":
                     revenue = calc_revenue(demand, cost)
-                    if transport_class in param.transit_classes:
-                        revenues_transit += revenue
-                        results["transit_revenue"] += vol_fac * revenue
-                    if transport_class in param.assignment_modes:
-                        revenues_car += revenue
-                        results["car_revenue"] += vol_fac * revenue
+                    revenues_transit += revenue
+                    results["transit_revenue"] += vol_fac * revenue
+                if mtx_type == "toll_cost":
+                    revenue = calc_revenue(demand, cost)
+                    revenues_car += revenue
+                    results["car_revenue"] += vol_fac * revenue
+                ws = workbook[TRANSLATIONS[transport_class]]
+                row_type = "cost" if mtx_type == "toll_cost" else mtx_type
+                ws[cols[timeperiod]+rows[row_type][0]] = gains_existing.sum()
+                ws[cols[timeperiod]+rows[row_type][1]] = gains_additional.sum()
             log.info(f"Mode {transport_class} calculated for {timeperiod}")
         ws = workbook["Tuottajahyodyt"]
         rows = CELL_INDICES["transit_revenue"]["rows"][year]
@@ -302,18 +311,6 @@ def read(file_name, scenario_path):
     """Read data from file."""
     return pandas.read_csv(
         Path(scenario_path, file_name), delim_whitespace=True)
-
-
-def read_costs(matrixdata, time_period, transport_class, mtx_type):
-    mtx_label = transport_class.split('_')[0]
-    ass_class = mtx_label if mtx_label == "bike" else transport_class
-    if mtx_label == "bike" and mtx_type == "cost":
-        matrix = 0
-    else:
-        with matrixdata.open(mtx_type, time_period) as mtx:
-            matrix = mtx[ass_class]
-            zone_numbers = mtx.zone_numbers
-    return matrix
 
 
 def calc_gains(demands, costs):

Scripts/assignment/abstract_assignment.py
@@ -59,9 +59,7 @@ class Period:
         pass
 
     @abstractmethod
-    def get_matrix(self,
-                    ass_class: str,
-                    matrix_type: str) -> numpy.ndarray:
+    def get_matrix(self, ass_class: str) -> numpy.ndarray:
         pass
 
     @abstractmethod

Scripts/assignment/datatypes/assignment_mode.py
@@ -1,0 +1,168 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING, Dict
+from abc import ABC, abstractmethod
+import numpy
+
+import parameters.assignment as param
+from assignment.datatypes.path_analysis import PathAnalysis
+from assignment.datatypes.emme_matrix import EmmeMatrix, PermanentEmmeMatrix
+if TYPE_CHECKING:
+    from assignment.assignment_period import AssignmentPeriod
+
+
+LENGTH_ATTR = "length"
+
+
+class AssignmentMode(ABC):
+    """Abstract base class for specifying mode assignment and matrices.
+
+    A mode is assignment-period-specific, so that each mode-period
+    combination has its own matrices.
+
+    All modes have one demand matrix, which is "permanent".
+    This means that it cannot be initialized or cleared after creation,
+    so that additional demand can safely be added to it at any time.
+    Demand initialisation is done by setting matrix to zero.
+    """
+    def __init__(self, name: str, assignment_period: AssignmentPeriod,
+                 save_matrices: bool = False):
+        """Initialize mode.
+
+        Parameters
+        ----------
+        name : str
+            Mode name
+        assignment_period : AssignmentPeriod
+            Assignment period to link to the mode
+        save_matrices : bool (optional)
+            Whether matrices will be saved in Emme format for all time periods
+        """
+        self.name = name
+        self.emme_scenario = assignment_period.emme_scenario
+        self.emme_project = assignment_period.emme_project
+        self.time_period = assignment_period.name
+        self._save_matrices = save_matrices
+        self._matrices: Dict[str, EmmeMatrix] = {}
+        self.demand = PermanentEmmeMatrix(
+            "demand", f"demand_{self.name}_{self.time_period}",
+            self.emme_project, self.emme_scenario.id, default_value=0)
+
+    def _create_matrix(self, mtx_type: str, default_value: float = 999999):
+        args = (
+            mtx_type, f"{mtx_type}_{self.name}_{self.time_period}",
+            self.emme_project, self.emme_scenario.id, default_value)
+        mtx = (PermanentEmmeMatrix(*args) if self._save_matrices
+               else EmmeMatrix(*args))
+        self._matrices[mtx_type] = mtx
+        return mtx
+
+    def init_matrices(self):
+        for mtx in self._matrices.values():
+            mtx.init()
+
+    def _release_matrices(self):
+        for mtx in self._matrices.values():
+            mtx.release()
+
+    @abstractmethod
+    def get_matrices(self) -> Dict[str, numpy.ndarray]:
+        """Get all LOS matrices.
+
+        Return
+        ------
+        dict
+            key : str
+                LOS type (time/cost/...)
+            value : numpy.ndarray
+                2-D matrix in float32
+        """
+        pass
+
+
+class SoftMode(AssignmentMode):
+    def __init__(self, *args, **kwargs):
+        AssignmentMode.__init__(self, *args, **kwargs)
+        self.dist = self._create_matrix("dist")
+        self.time = self._create_matrix("time")
+        self._specify()
+
+    def _specify(self):
+        pass
+
+    def get_matrices(self):
+        mtxs = {**self.dist.item, **self.time.item}
+        self._release_matrices()
+        return mtxs
+
+
+class BikeMode(SoftMode):
+    def _specify(self):
+        self.spec = {
+            "type": "STANDARD_TRAFFIC_ASSIGNMENT",
+            "classes": [
+                {
+                    "mode": param.main_mode,
+                    "demand": self.demand.id,
+                    "results": {
+                        "od_travel_times": {
+                            "shortest_paths": self.time.id,
+                        },
+                        "link_volumes": f"@{self.name}_{self.time_period}",
+                    },
+                    "analysis": {
+                        "results": {
+                            "od_values": self.dist.id,
+                        },
+                    },
+                }
+            ],
+            "path_analysis": PathAnalysis(LENGTH_ATTR).spec,
+            "stopping_criteria": {
+                "max_iterations": 1,
+                "best_relative_gap": 1,
+                "relative_gap": 1,
+                "normalized_gap": 1,
+            },
+            "performance_settings": param.performance_settings
+        }
+
+
+class WalkMode(SoftMode):
+    def _specify(self):
+        self.spec = {
+            "type": "STANDARD_TRANSIT_ASSIGNMENT",
+            "modes": param.aux_modes,
+            "demand": self.demand.id,
+            "waiting_time": {
+                "headway_fraction": 0.01,
+                "effective_headways": "hdw",
+                "perception_factor": 0,
+            },
+            "boarding_time": {
+                "penalty": 0,
+                "perception_factor": 0,
+            },
+            "aux_transit_time": {
+                "perception_factor": 1,
+            },
+            "od_results": {
+                "transit_times": self.time.id,
+            },
+            "strategy_analysis": {
+                "sub_path_combination_operator": "+",
+                "sub_strategy_combination_operator": "average",
+                "trip_components": {
+                    "aux_transit": "length",
+                },
+                "selected_demand_and_transit_volumes": {
+                    "sub_strategies_to_retain": "ALL",
+                    "selection_threshold": {
+                        "lower": None,
+                        "upper": None,
+                    },
+                },
+                "results": {
+                    "od_values": self.dist.id,
+                },
+            },
+        }

Scripts/assignment/datatypes/car.py
@@ -1,67 +1,102 @@
 from __future__ import annotations
+from typing import TYPE_CHECKING, Union
+
+
 import parameters.assignment as param
+from assignment.datatypes.assignment_mode import AssignmentMode, LENGTH_ATTR
 from assignment.datatypes.path_analysis import PathAnalysis
-from collections.abc import Callable
-from typing import Any, Dict, Optional, Union
+if TYPE_CHECKING:
+    from assignment.assignment_period import AssignmentPeriod
 
 
-LENGTH_ATTR = "length"
-
-
-class Car:
-    def __init__(self,
-                 ass_class: str,
-                 extra: Callable,
-                 emme_matrices: Dict[str, Union[str, Dict[str, str]]],
-                 link_costs: Union[str, float]):
-        """Car assignment class definition.
+class CarMode(AssignmentMode):
+    def __init__(self, name: str, assignment_period: AssignmentPeriod,
+                 dist_unit_cost: float, include_toll_cost: bool,
+                 save_matrices: bool = False):
+        """Initialize car mode.
 
         Parameters
         ----------
-        ass_class : str
-            Assignment class (car_work/car_leisure/van/truck/trailer_truck)
-        extra : assignment_period.AssignmentPeriod.extra()
-            Function for generating extra attribute name
-            for specific assignment period
-        emme_matrices : dict
-            key : str
-                Impedance type (time/cost/dist/...)
-            value : str
-                Emme matrix id
-        link_costs : str or float
-            Extra attribute where link cost is found (str) or length
-            multiplier to calculate link cost (float)
+        name : str
+            Mode name
+        assignment_period : AssignmentPeriod
+            Assignment period to link to the mode
+        dist_unit_cost : float
+            Length multiplier to calculate link cost
+        include_toll_cost : bool
+            Whether network links have "hinta" attribute defined
+        save_matrices : bool (optional)
+            Whether matrices will be saved in Emme format for all time periods
         """
-        perception_factor = param.vot_inv[param.vot_classes[ass_class]]
-        try:
-            perception_factor *= link_costs
-        except TypeError:
-            pass
+        AssignmentMode.__init__(self, name, assignment_period, save_matrices)
+        self.vot_inv = param.vot_inv[param.vot_classes[self.name]]
+        self.gen_cost = self._create_matrix("gen_cost")
+        self.dist = self._create_matrix("dist")
+        self.dist_unit_cost = dist_unit_cost
+        self._include_toll_cost = include_toll_cost
+        perception_factor = self.vot_inv
+        if include_toll_cost:
+            self.toll_cost = self._create_matrix("toll_cost")
+            self.link_cost_attr = f"@cost_{self.name[:10]}_{self.time_period}"
+            self.emme_project.create_extra_attribute(
+                "LINK", self.link_cost_attr, "total cost",
+                overwrite=True, scenario=self.emme_scenario)
         else:
-            link_costs = LENGTH_ATTR
-        self.spec: Dict[str, Any] = {
-            "mode": param.assignment_modes[ass_class],
-            "demand": emme_matrices["demand"],
+            perception_factor *= self.dist_unit_cost
+            self.link_cost_attr = LENGTH_ATTR
+
+        # Specify
+        self.spec = {
+            "mode": param.assignment_modes[self.name],
+            "demand": self.demand.id,
             "generalized_cost": {
-                "link_costs": link_costs,
+                "link_costs": self.link_cost_attr,
                 "perception_factor": perception_factor,
             },
             "results": {
-                "link_volumes": extra(ass_class),
+                "link_volumes": f"@{self.name}_{self.time_period}",
                 "od_travel_times": {
-                    "shortest_paths": emme_matrices["gen_cost"]
+                    "shortest_paths": self.gen_cost.id
                 }
             },
             "path_analyses": []
         }
-        self.add_analysis(LENGTH_ATTR, emme_matrices["dist"])
-        if link_costs != LENGTH_ATTR:
-            self.add_analysis(extra("toll_cost"), emme_matrices["cost"])
-        if ass_class in param.truck_classes:
-            self.add_analysis(extra("truck_time"), emme_matrices["time"])
-    
-    def add_analysis (self, 
-                      link_component: str, 
+        self.add_analysis(LENGTH_ATTR, self.dist.id)
+        if self._include_toll_cost:
+            self.add_analysis(
+                f"@toll_cost_{self.time_period}", self.toll_cost.id)
+
+    def add_analysis (self,
+                      link_component: str,
                       od_values: Union[int, str]):
         analysis = PathAnalysis(link_component, od_values)
         self.spec["path_analyses"].append(analysis.spec)
+
+    def get_matrices(self):
+        cost = self.dist_unit_cost * self.dist.data
+        if self._include_toll_cost:
+            cost += self.toll_cost.data
+        time = self._get_time(cost)
+        m = {"cost": cost, "time": time, **self.dist.item}
+        if self._include_toll_cost:
+            m.update(self.toll_cost.item)
+        self._release_matrices()
+        # fix the emme path analysis results
+        # (dist and cost are zero if path not found but we want it to
+        # be the default value 999999)
+        path_not_found = time > 999999
+        for mtx_type in ("cost", "dist"):
+            m[mtx_type][path_not_found] = 999999
+        return m
+
+    def _get_time(self, cost):
+        return self.gen_cost.data - self.vot_inv*cost
+
+class TruckMode(CarMode):
+    def __init__(self, *args, **kwargs):
+        CarMode.__init__(self, *args, **kwargs)
+        self.time = self._create_matrix("time")
+        self.add_analysis(f"@truck_time_{self.time_period}", self.time.id)
+
+    def _get_time(self, *args):
+        return self.time.data

Scripts/assignment/datatypes/car_specification.py
@@ -1,41 +1,23 @@
 from __future__ import annotations
-from typing import Any, Dict, List, Union
+from typing import Any, Dict
 import parameters.assignment as param
-from assignment.datatypes.car import Car
-from collections.abc import Callable
+from assignment.datatypes.car import CarMode
 
 class CarSpecification:
 
-    def __init__(self,
-                 extra: Callable, 
-                 emme_matrices: Dict[str, Union[str, Dict[str, str]]],
-                 link_costs: Dict[str, Union[str, float]]):
+    def __init__(self, modes: Dict[str, CarMode]):
         """
         Car assignment specification.
 
         Parameters
         ----------
-        extra : assignment_period.AssignmentPeriod.extra()
-            Function for generating extra attribute name
-            for specific assignment period
-        emme_matrices : dict
+        modes : dict
             key : str
                     Assignment class (car_work/transit_leisure/...)
-            value : dict
-                key : str
-                    Impedance type (time/cost/dist/...)
-                value : str
-                    Emme matrix id
-        link_costs : dict
-            key : str
-                Assignment class (car_work/truck/...)
-            value : str or float
-                Extra attribute where link cost is found (str) or length
-                multiplier to calculate link cost (float)
+            value : CarMode
+                Assignment mode to add to specification
         """
-        modes = param.assignment_modes.keys() & emme_matrices.keys()
-        self._modes = {m: Car(m, extra, emme_matrices[m], link_costs[m])
-                    for m in modes}
+        self._modes = modes
         self._spec = {
             "type": "SOLA_TRAFFIC_ASSIGNMENT",
             "background_traffic": {
@@ -47,12 +29,18 @@ class CarSpecification:
         }
 
     def light_spec(self) -> Dict[str, Any]:
-        self._spec["classes"] = [self._modes[mode].spec
-            for mode in param.car_classes + ("van",)]
+        specs = []
+        for mode in param.car_classes + ("van",):
+            self._modes[mode].init_matrices()
+            specs.append(self._modes[mode].spec)
+        self._spec["classes"] = specs
         return self._spec
 
     def truck_spec(self) -> Dict[str, Any]:
-        self._spec["classes"] = [self._modes[mode].spec
-            for mode in param.truck_classes]
+        specs = []
+        for mode in param.truck_classes:
+            self._modes[mode].init_matrices()
+            specs.append(self._modes[mode].spec)
+        self._spec["classes"] = specs
         return self._spec
 

Scripts/assignment/datatypes/emme_matrix.py
@@ -1,0 +1,97 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING, Dict
+import numpy
+
+import utils.log as log
+if TYPE_CHECKING:
+    from assignment.emme_bindings.emme_project import EmmeProject
+
+
+class EmmeMatrix:
+    """Container for EMME matrix.
+
+    To actually create the EMME matrix (which is a file),
+    `init` method must be called.
+    Disk storage can be released by calling `release`.
+    After release, `init` must be called again to be able to set the matrix.
+    """
+
+    id_counter = 0
+
+    def __init__(self, name: str, description: str, emme_project: EmmeProject,
+                 scenario_id: int, default_value: float = 99999):
+        """Initialize container.
+
+        Parameters
+        ----------
+        name : str
+            Short name (e.g., "time")
+        description : str
+            Long name used in EMME (e.g., "time_transit_work_aht")
+        emme_project : assignment.emme_bindings.emme_project.EmmeProject
+            Emme project connected to this assignment
+        scenario_id : int
+            Id of EMME scenario
+        default_value : float (optional)
+            Can be set to a number, otherwise it is 99999
+        """
+        EmmeMatrix.id_counter +=1
+        self._id = EmmeMatrix.id_counter
+        self.name = name
+        self.description = description
+        self.default_value = default_value
+        self._emme_project = emme_project
+        self._scenario_id = scenario_id
+
+    @property
+    def id(self):
+        return f"mf{self._id}"
+
+    def init(self):
+        """Create (overwrite if exists) matrix in EMME and set to default."""
+        emmebank = self._emme_project.modeller.emmebank
+        if emmebank.matrix(self.id) is not None:
+            emmebank.delete_matrix(self.id)
+        self._emme_project.create_matrix(
+            self.id, self.description, self.description, self.default_value)
+
+    def set(self, matrix: numpy.ndarray):
+        if numpy.isnan(matrix).any():
+            msg = ("NAs in demand matrix "
+                   + "would cause infinite loop in Emme assignment.")
+            log.error(msg)
+            raise ValueError(msg)
+        else:
+            (self._emme_project.modeller.emmebank.matrix(self.id)
+             .set_numpy_data(matrix, scenario_id=self._scenario_id))
+
+    @property
+    def data(self) -> numpy.ndarray:
+        return (self._emme_project.modeller.emmebank.matrix(self.id)
+               .get_numpy_data(scenario_id=self._scenario_id))
+
+    @property
+    def item(self):
+        return {self.name: self.data}
+
+    def release(self):
+        """Remove matrix from EMME if not `PermanentEmmeMatrix.`"""
+        self._emme_project.modeller.emmebank.delete_matrix(self.id)
+
+
+class PermanentEmmeMatrix(EmmeMatrix):
+    """Container for EMME matrix.
+
+    This immediately creates the EMME matrix (which is a file).
+    It will exist through the whole model run, and calling `release`
+    will not affect that.
+    """
+    def __init__(self, *args, **kwargs):
+        EmmeMatrix.__init__(self, *args, **kwargs)
+        EmmeMatrix.init(self)
+
+    def init(self):
+        pass
+
+    def release(self):
+        pass

Scripts/assignment/datatypes/freight_specification.py
@@ -1,32 +1,21 @@
 from __future__ import annotations
-from typing import Any, Dict, Union
+from typing import Any, Dict
 
 import parameters.assignment as param
+from assignment.datatypes.assignment_mode import AssignmentMode
 
 
-class FreightSpecification:
-    """
-    Freight assignment specification.
-
-    Parameters
-    ----------
-    modes : dict
-        key : str
-            One-letter mode ID
-        value : str
-            Terminal cost attribute name
-    emme_matrices : dict
-        key : str
-            Impedance type (time/cost/dist/...)
-        value : str
-            Emme matrix id
-    """
-    def __init__(self,
-                 modes: Dict[str, str],
-                 emme_matrices: Dict[str, Union[str, Dict[str, str]]]):
+class FreightMode(AssignmentMode):
+    def __init__(self, *args, **kwargs):
+        AssignmentMode.__init__(self, *args, **kwargs)
+        self.dist = self._create_matrix("dist")
+        self.aux_dist = self._create_matrix("aux_dist")
+        self.time = self._create_matrix("time")
+        self.aux_time = self._create_matrix("aux_time")
         no_penalty = dict.fromkeys(
             ["global", "at_nodes", "on_lines", "on_segments"])
         all_modes = {param.park_and_ride_mode: "truck access"}
+        modes = param.freight_modes[self.name]
         all_modes.update(modes)
         transitions = [{
             "mode": mode,
@@ -56,7 +45,7 @@ class FreightSpecification:
         self.spec: Dict[str, Any] = {
             "type": "EXTENDED_TRANSIT_ASSIGNMENT",
             "modes": list(all_modes),
-            "demand": emme_matrices["demand"],
+            "demand": self.demand.id,
             "waiting_time": {
                 "headway_fraction": 0.1,
                 "effective_headways": "hdw",
@@ -87,21 +76,31 @@ class FreightSpecification:
             "type": "EXTENDED_TRANSIT_MATRIX_RESULTS",
             "by_mode_subset": {
                 "modes": list(modes),
-                "distance": emme_matrices["dist"],
-                "actual_in_vehicle_times": emme_matrices["time"],
+                "distance": self.dist.id,
+                "actual_in_vehicle_times": self.time.id,
             },
         }
         self.local_result_spec = {
             "type": "EXTENDED_TRANSIT_MATRIX_RESULTS",
             "by_mode_subset": {
                 "modes": [param.park_and_ride_mode],
-                "distance": emme_matrices["aux_dist"],
-                "actual_aux_transit_times": emme_matrices["aux_time"],
+                "distance": self.aux_dist.id,
+                "actual_aux_transit_times": self.aux_time.id,
             },
         }
         self.ntw_results_spec = {
             "type": "EXTENDED_TRANSIT_NETWORK_RESULTS",
-            "analyzed_demand": emme_matrices["demand"],
+            "analyzed_demand": self.demand.id,
             "on_links": {},
             "on_segments": {},
         }
+
+    def get_matrices(self):
+        mtxs = {
+            **self.dist.item,
+            **self.aux_dist.item,
+            **self.time.item,
+            **self.aux_time.item,
+        }
+        self._release_matrices()
+        return mtxs

commit 9bb03c74ce8ccf4c7f1a66a0a0cb63dea6e74bd0

Fix CBA things that were broken in #123

Scripts/cba.py
@@ -270,7 +270,7 @@ def run_cost_benefit_analysis(scenario_0, scenario_1, year, workbook, submodel):
                         except NoSuchNodeError:
                             matrices_found = False
                 if not matrices_found:
-                    break
+                    continue
                 if transport_class == "train" and mtx_type == "dist":
                     # Max travel time with fixed 20 kmph speed
                     # and one-hour start time
@@ -282,7 +282,8 @@ def run_cost_benefit_analysis(scenario_0, scenario_1, year, workbook, submodel):
                 result_type = transport_class + "_" + mtx_type
                 results[result_type] += (vol_fac *
                                          (gains_existing+gains_additional))
-                if mtx_type == "cost":
+                if (mtx_type == "cost"
+                        and transport_class in param.transit_classes):
                     revenue = calc_revenue(demand, cost)
                     revenues_transit += revenue
                     results["transit_revenue"] += vol_fac * revenue
@@ -301,7 +302,10 @@ def run_cost_benefit_analysis(scenario_0, scenario_1, year, workbook, submodel):
         ws = workbook["Julkistaloudelliset"]
         cols = CELL_INDICES["car_revenue"]["cols"]
         rows = CELL_INDICES["car_revenue"]["rows"][year]
-        ws[cols[timeperiod]+rows] = revenues_car.sum()
+        try:
+            ws[cols[timeperiod]+rows] = revenues_car.sum()
+        except AttributeError:
+            pass
         log.info("Gains and revenues calculated for {}".format(timeperiod))
     log.info("Year {} completed".format(year))
     return pandas.DataFrame(results, zone_numbers)