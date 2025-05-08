3eba9da69a5d6d0d88b73b7703b8d1fcd81276f7
Elias Marttunen <ext-elias.marttunen@traficom.fi>
2025-05-08 17:02:17 +0300

    Fix tests

--- Scripts/tests/integration/test_models.py
+++ Scripts/tests/integration/test_models.py
@@ -54,7 +54,7 @@ class ModelTest(unittest.TestCase):
         # Check that model result does not change
         self.assertAlmostEquals(
             model.mode_share[0]["car_work"] + model.mode_share[0]["car_leisure"],
-            0.32151093831718947)
+            0.31724308032464517)
         
         print("Model system test done")
 
