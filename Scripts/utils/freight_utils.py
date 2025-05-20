c1414652d74922bdef897538388525c153b21a51
Konsta Tarkkala <103411855+Konnde@users.noreply.github.com>
2025-05-20 14:16:31 +0300

    Handle zone system related conflicts during storing (#236)
    * Add new StoreDemand class for handling storing demand with assessing needs for demand to be padded with zeros

--- Scripts/utils/freight_utils.py
+++ Scripts/utils/freight_utils.py
@@ -1,4 +1,5 @@
 import json
+import numpy
 from pathlib import Path
 from typing import Dict
 
@@ -7,6 +8,8 @@ from datatypes.purpose import FreightPurpose
 from datahandling.zonedata import FreightZoneData
 from datahandling.resultdata import ResultsData
 from parameters.commodity import commodity_conversion
+from datahandling.matrixdata import MatrixData
+from assignment.freight_assignment import FreightAssignmentPeriod
     
 
 def create_purposes(parameters_path: Path, zonedata: FreightZoneData, 
@@ -46,3 +49,64 @@ def create_purposes(parameters_path: Path, zonedata: FreightZoneData,
             log.warn(f"Aggregated commodity class '{commodity_conversion[commodity]}' "
                       f"for commodity {commodity} not found in costs json.")
     return purposes
+
+class StoreDemand():
+    """Handles demand dimension compatibility when storing demand matrices 
+    into Emme and omx-files.
+    """
+
+    def __init__(self, 
+                 freight_network: FreightAssignmentPeriod, 
+                 resultmatrices: MatrixData, 
+                 all_zone_numbers: numpy.ndarray, 
+                 zone_numbers: numpy.ndarray):
+        self.network = freight_network
+        self.resultmatrices = resultmatrices
+        self.all_zones = all_zone_numbers
+        self.zones = zone_numbers
+
+    def store(self, mode: str, demand: numpy.ndarray, 
+              omx_filename: str = "", key_prefix: str = ""):
+        """Stores demand matrices into Emme and as omx if user has given
+        name for the .omx file. 
+
+        Parameters
+        ----------
+        mode : str
+            freight mode/assignment class
+        demand : numpy.ndarray
+            matrix that is set to Emme
+        omx_filename : str, by default empty string
+            optional name of an external .omx file for saving results
+        key_prefix : str, by default empty string
+            optional name prefix for matrix e.g. purpose name
+        """
+        emme_mtx = self.assess_dimensions(demand)
+        self.network.set_matrix(mode, emme_mtx)
+        if omx_filename:
+            with self.resultmatrices.open(omx_filename, self.network.name, 
+                                          self.all_zones, m="a") as mtx:
+                keyname = f"{key_prefix}_{mode}" if key_prefix else mode
+                mtx[keyname] = emme_mtx
+
+    def assess_dimensions(self, demand: numpy.ndarray) -> numpy.ndarray:
+        """Evaluates whether given demand matrix needs to be padded with zones 
+        to maintain zone compatibility with scenario's Emme network.
+
+        Parameters
+        ----------
+        demand : numpy.ndarray
+            type demand matrix which is assessed before setting into Emme
+
+        Returns
+        -------
+        numpy.ndarray
+            demand with/without zone padding
+        """
+        fill_mtx = demand
+        nr_all_zones = self.all_zones.size
+        nr_zones = self.zones.size
+        if demand.size != nr_all_zones**2:
+            fill_mtx = numpy.zeros([nr_all_zones, nr_all_zones], dtype=numpy.float32)
+            fill_mtx[:nr_zones, :nr_zones] = demand
+        return fill_mtx
