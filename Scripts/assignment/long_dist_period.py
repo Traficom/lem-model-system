3c7895d43c373bac317f3a660e1d95abb0057fee
Jens West <jens.west@traficom.fi>
2025-06-06 15:59:38 +0300

    fix syntax

--- Scripts/assignment/long_dist_period.py
+++ Scripts/assignment/long_dist_period.py
@@ -103,5 +103,5 @@ class WholeDayPeriod(AssignmentPeriod):
             if self._delete_strat_files:
                 strategy_paths[transit_class].unlink(missing_ok=True)
         self._calc_transit_link_results()
-        return self._get_impedances((param.car_classes
+        return self._get_impedances(param.car_classes
                                     + param.long_distance_transit_classes)
