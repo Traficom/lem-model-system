b77c75969d592f31a06a5cafc8cb7cd17e934fe9
attesn <atte.supponen@outlook.com>
2025-08-21 16:19:12 +0300

    Remove double counting of logsums in estimation

--- Scripts/tests/integration/test_models.py
+++ Scripts/tests/integration/test_models.py
@@ -74,7 +74,7 @@ class ModelTest(unittest.TestCase):
         # Check that model result does not change
         self.assertAlmostEquals(
             model.mode_share[0]["car_work"] + model.mode_share[0]["car_leisure"],
-            0.6857780969643223)
+            0.6915877028187531)
 
     def _validate_impedances(self, impedances):
         self.assertIsNotNone(impedances)
