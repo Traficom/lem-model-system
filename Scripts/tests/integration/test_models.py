627bacb37d0fea84a7a8cf678fc00e70f49972be
attesn <atte.supponen@outlook.com>
2025-09-11 09:42:56 +0300

    Update distance limit for other shopping tours Quickfix to problems with cost parameters

--- Scripts/tests/integration/test_models.py
+++ Scripts/tests/integration/test_models.py
@@ -54,7 +54,7 @@ class ModelTest(unittest.TestCase):
         # Check that model result does not change
         self.assertAlmostEquals(
             model.mode_share[0]["car_work"] + model.mode_share[0]["car_leisure"],
-            0.33452266190022867)
+            0.33492949606539296)
         
         print("Model system test done")
 
