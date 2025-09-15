3b91ed3e4041b03a6239a35effb13b9d59c10986
attesn <atte.supponen@outlook.com>
2025-09-15 15:41:05 +0300

    Update integration test mode share

--- Scripts/tests/integration/test_models.py
+++ Scripts/tests/integration/test_models.py
@@ -74,7 +74,7 @@ class ModelTest(unittest.TestCase):
         # Check that model result does not change
         self.assertAlmostEquals(
             model.mode_share[0]["car_work"] + model.mode_share[0]["car_leisure"],
-            0.7016409205609215)
+            0.7011378312483222)
 
     def _validate_impedances(self, impedances):
         self.assertIsNotNone(impedances)
