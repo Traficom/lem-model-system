9fc77cc19cc09568be9549f5482d7df7c0692927
attesn <atte.supponen@outlook.com>
2025-10-15 16:21:11 +0300

    Update student tour generation parameters Missing data in estimation fixed and population density removed

--- Scripts/tests/integration/test_models.py
+++ Scripts/tests/integration/test_models.py
@@ -54,7 +54,7 @@ class ModelTest(unittest.TestCase):
         # Check that model result does not change
         self.assertAlmostEquals(
             model.mode_share[0]["car_work"] + model.mode_share[0]["car_leisure"],
-            0.3356082436780635)
+            0.34039267890216823)
         
         print("Model system test done")
 
